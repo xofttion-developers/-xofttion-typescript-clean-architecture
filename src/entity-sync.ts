77da8b146cb5dc986d493d0f4dea1f9e22cf0232
ing.dacastillop <ing.dacastillop@gmail.com>
2023-02-12 10:38:34 -0500

    :truck: Relocate EntityLink and EntitySync classes to separate files

---
+++ src/entity-sync.ts
@@ -0,0 +1,55 @@
+import { Entity } from './entity';
+import { BaseModel, ModelUpdated } from './model';
+
+export type ModelDirty = { [key: string]: any };
+
+export abstract class EntitySync<E extends Entity, M extends BaseModel> {
+  private firstStatus: ModelDirty;
+
+  constructor(public readonly entity: E, public readonly model: M) {
+    this.firstStatus = this.mapModel(model);
+  }
+
+  public abstract sync(): void;
+
+  public verify(): ModelDirty | undefined {
+    this.sync();
+
+    return this.getDirty();
+  }
+
+  private mapModel(model: M): ModelDirty {
+    const dirty: ModelDirty = {};
+
+    Object.keys(model).forEach((key) => {
+      dirty[key] = (model as any)[key];
+    });
+
+    return dirty;
+  }
+
+  private getDirty(): ModelDirty | undefined {
+    const currentStatus = this.mapModel(this.model);
+
+    const modelDirty: ModelDirty = {};
+
+    let dirty = false;
+
+    Object.keys(currentStatus).forEach((key) => {
+      if (currentStatus[key] !== this.firstStatus[key]) {
+        dirty = true;
+        modelDirty[key] = currentStatus[key];
+      }
+    });
+
+    if (isUpdated(this.model)) {
+      modelDirty['updatedAt'] = new Date();
+    }
+
+    return dirty ? modelDirty : undefined;
+  }
+}
+
+function isUpdated(model: any): model is ModelUpdated {
+  return 'updatedAt' in model;
+}
